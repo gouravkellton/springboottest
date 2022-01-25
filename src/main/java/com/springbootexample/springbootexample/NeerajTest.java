180d0ba8f3cb90b118da8277b96ad074a6524e73
Neeraj Bhagat <neerajbhagat9872@gmail.com>
2022-01-25 20:48:37 +0530

    vfbgdgfgb nnn

--- src/main/java/com/springbootexample/springbootexample/NeerajTest.java
+++ src/main/java/com/springbootexample/springbootexample/NeerajTest.java
@@ -3,6 +3,7 @@ package com.springbootexample.springbootexample;
 public class NeerajTest {
 
 	public static void main(String[] args) {
-		System.out.println("I Love Riya");
+		System.out.println("I Love Riya, ");
+		System.out.println("Chal baag yaha se");
 	}
 }
